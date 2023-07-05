7653dd032d374c60885e8033ee7b61d9d00fe317
Rafael Pacheco <rafael.schwalbe@outlook.com>
2023-07-05 12:25:06 -0300

    Criada solução para o desafio de receber um numero inteiro e devolver o texto correspondente por extenso. Funcionando até 1 bilhão

---
+++ ChallengeLibrary/NumberToTextChallenge.cs
@@ -0,0 +1,96 @@
+﻿namespace ChallengeLibrary
+{
+    public static class NumberToTextChallenge
+    {
+        //Implemente uma função que recebe um número inteiro e retorne uma string com a representação por extenso desse número.Exemplo: 123 -> "cento e vinte e três"
+        //Como você implementou a função que retorna a representação por extenso do número no desafio 1? Quais foram os principais desafios encontrados?
+
+        public static string NumberToText(int number)
+        {
+            if (number == 0)
+            {
+                return "zero";
+            }
+
+            if (number == 100)
+            {
+                return "cem";
+            }
+
+            if (number == 1000000000)
+            {
+                return "um bilhão";
+            }
+
+            if (number < 0 || number > 1000000000)
+            {
+                return "Número fora do intervalo suportado.";
+            }
+
+            string[] units = { "", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove" };
+            string[] dozens = { "", "dez", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa" };
+            string[] hundreds = { "", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos" };
+            string[] thousands = { "", "mil", "milhão", "bilhão", "trilhão", "quadrilhão", "quintilhão", "sextilhão", "septilhão", "octilhão", "nonilhão", "decilhão" };
+
+            if (number < 20)
+            {
+                return units[number];
+            }
+
+            if (number < 100)
+            {
+                int dozen = number / 10;
+                int unit = number % 10;
+
+                if (unit == 0)
+                {
+                    return dozens[dozen];
+                }
+                else
+                {
+                    return dozens[dozen] + " e " + units[unit];
+                }
+            }
+
+            if (number < 1000)
+            {
+                int hundred = number / 100;
+                int residue = number % 100;
+
+                if (residue == 0)
+                {
+                    return hundreds[hundred];
+                }
+                else
+                {
+                    return hundreds[hundred] + " e " + NumberToText(residue);
+                }
+            }
+
+            if (number < 1000000000)
+            {
+                for (int i = 1; i <= thousands.Length - 1; i++)
+                {
+                    int divisor = (int)Math.Pow(1000, i);
+                    if (number < divisor * 1000)
+                    {
+                        int prefix = number / divisor;
+                        int suffix = number % divisor;
+
+                        string result = NumberToText(prefix) + " " + thousands[i];
+
+                        if (suffix > 0)
+                        {
+                            result += " e " + NumberToText(suffix);
+                        }
+
+                        return result;
+                    }
+                }
+            }
+
+            return "Número fora do intervalo válido.";
+        }
+
+    }
+}
--- TestProject/UnitTests.cs
+++ TestProject/UnitTests.cs
@@ -6,6 +6,69 @@ namespace TestProject
     public class UnitTests
     {
         [TestMethod]
+        public void NumberToTextTestMethod()
+        {
+            string result;
+
+            result = NumberToTextChallenge.NumberToText(0);
+            Assert.AreEqual("zero", result);
+
+            result = NumberToTextChallenge.NumberToText(100);
+            Assert.AreEqual("cem", result);
+
+            result = NumberToTextChallenge.NumberToText(15);
+            Assert.AreEqual("quinze", result);
+
+            result = NumberToTextChallenge.NumberToText(20);
+            Assert.AreEqual("vinte", result);
+
+            result = NumberToTextChallenge.NumberToText(35);
+            Assert.AreEqual("trinta e cinco", result);
+
+            result = NumberToTextChallenge.NumberToText(123);
+            Assert.AreEqual("cento e vinte e três", result);
+
+            result = NumberToTextChallenge.NumberToText(795);
+            Assert.AreEqual("setecentos e noventa e cinco", result);
+            
+            result = NumberToTextChallenge.NumberToText(1000);
+            Assert.AreEqual("um mil", result);
+
+            result = NumberToTextChallenge.NumberToText(10000);
+            Assert.AreEqual("dez mil", result);
+
+            result = NumberToTextChallenge.NumberToText(10001);
+            Assert.AreEqual("dez mil e um", result);
+
+            result = NumberToTextChallenge.NumberToText(11018);
+            Assert.AreEqual("onze mil e dezoito", result);
+
+            result = NumberToTextChallenge.NumberToText(11029);
+            Assert.AreEqual("onze mil e vinte e nove", result);
+
+            result = NumberToTextChallenge.NumberToText(611329);
+            Assert.AreEqual("seiscentos e onze mil e trezentos e vinte e nove", result);
+            
+            result = NumberToTextChallenge.NumberToText(1611329);
+            Assert.AreEqual("um milhão e seiscentos e onze mil e trezentos e vinte e nove", result);
+            
+            result = NumberToTextChallenge.NumberToText(20611329);
+            Assert.AreEqual("vinte milhão e seiscentos e onze mil e trezentos e vinte e nove", result);
+            
+            result = NumberToTextChallenge.NumberToText(123456789);
+            Assert.AreEqual("cento e vinte e três milhão e quatrocentos e cinquenta e seis mil e setecentos e oitenta e nove", result);
+            
+            result = NumberToTextChallenge.NumberToText(999999999);
+            Assert.AreEqual("novecentos e noventa e nove milhão e novecentos e noventa e nove mil e novecentos e noventa e nove", result);
+
+            result = NumberToTextChallenge.NumberToText(1000000000);
+            Assert.AreEqual("um bilhão", result);
+
+            //result = NumberToTextChallenge.NumeroPorExtenso(int.MaxValue);//2.147.483.647
+            //Assert.AreEqual("dois bilhão e cento e quarenta e sete milhão quatrocentos e oitenta e três mil e seiscentos e quarenta e sete", result);
+        }
+
+        [TestMethod]
         public void SumChallengeTestMethod()
         {
             var inArray = new List<int>();
