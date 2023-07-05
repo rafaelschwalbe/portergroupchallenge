b4b42a25d6646fea4fd3857c247452a7355762a6
Rafael Pacheco <rafael.schwalbe@outlook.com>
2023-07-05 17:44:32 -0300

    criada CleanListChallenge, ainda sem teste

---
+++ ChallengeLibrary/CleanListChallenge.cs
@@ -0,0 +1,17 @@
+﻿namespace ChallengeLibrary
+{
+    public static class CleanListChallenge
+    {
+        //Implemente uma função que recebe uma lista de objetos e retorne uma nova lista apenas com os objetos únicos, ou seja, sem repetições.
+        public static List<int> RemoveDuplicatedItems(List<int> list)
+        {
+            List<int> result = new List<int>();
+            foreach (int item in list)
+            {
+                if (list.Count(c => c == item) == 1)
+                    result.Add(item);
+            }
+            return result;
+        }
+    }
+}
